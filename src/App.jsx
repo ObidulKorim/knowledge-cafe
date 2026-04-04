3eb4c960a5cf6d3144c569b8f598fe0502b6f596
obidulkorim <mrsthwo@gmail.com>
2026-04-04 23:13:41 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/header/header/header';
+import Blogs from './components/header/blogs/blogs';
+
+function App() {
+
+
+  return (
+    <>
+    <Header></Header>
+    <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/components/header/header/header.jsx
@@ -0,0 +1,11 @@
+import profile from '../../../assets/images/profile.png'
+const Header = () => {
+    return (
+        <div className='container mx-auto flex justify-between items-center border-b-2 py-5 px-36'>
+            <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </div>
+    );
+ };
+ 
+ export default Header;
